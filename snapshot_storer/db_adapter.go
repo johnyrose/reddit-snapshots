495154f929ff9dd5a60e9fb926efab1a1403ef5d
Jonathan Rosenboim <masterfqf@gmail.com>
2018-12-13 21:25:10 +0200

    Adds the ConnectToDatabase function.

--- snapshot_storer/db_adapter.go
+++ snapshot_storer/db_adapter.go
@@ -1,5 +1,18 @@
 package snapshot_storer
 
+import (
+	"github.com/globalsign/mgo"
+	"log"
+)
+
 type MongoClient struct {
 	Url string
+}
+
+func (c MongoClient) ConnectToDatabase() mgo.Session {
+	client, err := mgo.Dial(c.Url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return *client
 }
