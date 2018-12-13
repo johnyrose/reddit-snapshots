2ad795919cba6d5e3a9ccd1529cc915fd9403372
Jonathan Rosenboim <masterfqf@gmail.com>
2018-12-13 21:58:24 +0200

    Adds configuration variables.

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "os"
+
+var (
+	clientID            = os.Getenv("CLIENT_ID")
+	clientSecret        = os.Getenv("CLIENT_SECRET")
+	username            = os.Getenv("USERNAME")
+	password            = os.Getenv("PASSWORD")
+	dbUrl               = os.Getenv("DB_URL")
+	dbName              = os.Getenv("DB_NAME")
+	snapshotsCollection = os.Getenv("SNAPSHOTS_COLLECTION")
+	configCollection    = os.Getenv("CONFIG_COLLECTION")
+)
+
 func main() {
 
 }
