b458bf17f4f66f1932815e2fcc1faeab1a5a2af5
Jonathan Rosenboim <masterfqf@gmail.com>
2018-12-13 21:18:33 +0200

    Adds the generateSession function.

--- reddit_snapshot_catcher/reddit_client.go
+++ reddit_snapshot_catcher/reddit_client.go
@@ -1,8 +1,31 @@
 package reddit_snapshot_catcher
 
+import (
+	"log"
+
+	"github.com/jzelinskie/reddit"
+)
+
 type RedditClient struct {
 	ClientID     string
 	ClientSecret string
 	Username     string
 	Password     string
+}
+
+func (c RedditClient) generateSession() *geddit.OAuthSession {
+	o, err := geddit.NewOAuthSession(
+		c.ClientID,
+		c.ClientSecret,
+		"test",
+		"http://redirect.url",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = o.LoginAuth(c.Username, c.Password)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return o
 }
