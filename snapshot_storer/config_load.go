ecd2af09c57cc7068ad59c62359b2a8d5baa54a9
Jonathan Rosenboim <masterfqf@gmail.com>
2018-12-13 22:00:02 +0200

    Adds the loadConfigurationFromDB function.

--- snapshot_storer/config_load.go
+++ snapshot_storer/config_load.go
@@ -1,8 +1,22 @@
 package snapshot_storer
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"gopkg.in/mgo.v2/bson"
+	"log"
+)
 
 type Config struct {
 	ID         bson.ObjectId
 	Subreddits []bson.M
+}
+
+func loadConfigurationFromDB(c MongoClient, dbName string, collectionName string) Config {
+	client := c.ConnectToDatabase()
+	collection := client.DB(dbName).C(collectionName)
+	var result Config
+	err := collection.Find(nil).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return result
 }
