695b50c4fda584f51ad051d3d7182dbba218b005
Jonathan Rosenboim <masterfqf@gmail.com>
2018-12-13 21:21:16 +0200

    Adds the ToBsonM function.

--- reddit_snapshot_catcher/snapshot_catcher.go
+++ reddit_snapshot_catcher/snapshot_catcher.go
@@ -1,9 +1,26 @@
 package reddit_snapshot_catcher
 
-import "github.com/jzelinskie/reddit"
+import (
+	"github.com/jzelinskie/reddit"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+)
 
 type SubredditSnapshot struct {
 	Subreddit string
 	Time      string
 	Posts     []*geddit.Submission
+}
+
+func (s SubredditSnapshot) ToBsonM() bson.M {
+	data, err := bson.Marshal(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	m := make(bson.M)
+	err = bson.Unmarshal(data, &m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return m
 }
