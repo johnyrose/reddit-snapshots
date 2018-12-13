c5bd89d31b0251c7b2a1e18e773ad229d1618d64
Jonathan Rosenboim <masterfqf@gmail.com>
2018-12-13 21:38:33 +0200

    Changes import to be from GitHub.

--- snapshot_storer/snapshot_storer.go
+++ snapshot_storer/snapshot_storer.go
@@ -1,8 +1,10 @@
 package snapshot_storer
 
-import "reddit-take/reddit_snapshot_taker"
+import (
+	"github.com/Ripolak/reddit-snapshots/reddit_snapshot_catcher"
+)
 
-func StoreItem(subreddit reddit_snapshot_taker.SubredditSnapshot, mongoUrl string, dbName string, collection string) {
+func StoreItem(subreddit reddit_snapshot_catcher.SubredditSnapshot, mongoUrl string, dbName string, collection string) {
 	c := MongoClient{
 		Url: mongoUrl,
 	}
