30f0aa6b8e81e02d973dc926f80463abedd0dcef
Jonathan Rosenboim <masterfqf@gmail.com>
2018-12-15 15:48:39 +0200

    Adds the takeSnapshots function.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"gopkg.in/mgo.v2/bson"
 	"os"
 	"sync"
@@ -38,10 +39,21 @@ func main() {
 	}
 }
 
-func fetchSnapshots(subreddits []bson.M) {
+func fetchSnapshots(subreddits []bson.M, redditClient reddit_snapshot_catcher.RedditClient) {
 	var wg sync.WaitGroup
 	wg.Add(len(subreddits) * 2)
-
 	ch := make(chan reddit_snapshot_catcher.SubredditSnapshot, len(subreddits))
+	takeSnapshots(subreddits, &wg, redditClient, ch)
 	wg.Wait()
+}
+
+func takeSnapshots(subreddits []bson.M, wg *sync.WaitGroup, redditClient reddit_snapshot_catcher.RedditClient,
+	ch chan reddit_snapshot_catcher.SubredditSnapshot) {
+	for _, subreddit := range subreddits {
+		go func(subreddit string) {
+			defer wg.Done()
+			snapshot := reddit_snapshot_catcher.TakeSnapshot(redditClient, subreddit, "hot")
+			ch <- snapshot
+		}(subreddit["subreddit"].(string))
+	}
 }
