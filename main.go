a96f2f825e5323206e8d69cb80419a9756c834f5
Jonathan Rosenboim <masterfqf@gmail.com>
2018-12-15 17:32:50 +0200

    Moves functions to snapshots manager.

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"gopkg.in/mgo.v2/bson"
-	"os"
-	"sync"
-
 	"github.com/Ripolak/reddit-snapshots/reddit_snapshot_catcher"
 	"github.com/Ripolak/reddit-snapshots/snapshot_storer"
+	"os"
 )
 
 var (
@@ -30,36 +27,4 @@ func main() {
 	snapshotConfig := snapshot_storer.LoadConfiguration(dbUrl, dbName, configCollection)
 	subreddits := snapshotConfig.Subreddits
 	fetchSnapshots(subreddits)
-}
-
-func fetchSnapshots(subreddits []bson.M) {
-	var wg sync.WaitGroup
-	wg.Add(len(subreddits))
-	ch := make(chan reddit_snapshot_catcher.SubredditSnapshot, len(subreddits))
-	takeSnapshots(subreddits, &wg, ch)
-	wg.Wait()
-	close(ch)
-	wg.Add(len(subreddits))
-	storeSnapshots(ch, &wg)
-	wg.Wait()
-}
-
-func takeSnapshots(subreddits []bson.M, wg *sync.WaitGroup,
-	ch chan reddit_snapshot_catcher.SubredditSnapshot) {
-	for _, subreddit := range subreddits {
-		go func(subreddit string) {
-			defer wg.Done()
-			snapshot := reddit_snapshot_catcher.TakeSnapshot(reddit, subreddit, "hot")
-			ch <- snapshot
-		}(subreddit["subreddit"].(string))
-	}
-}
-
-func storeSnapshots(ch chan reddit_snapshot_catcher.SubredditSnapshot, wg *sync.WaitGroup) {
-	for msg := range ch {
-		go func(snap reddit_snapshot_catcher.SubredditSnapshot) {
-			defer wg.Done()
-			snapshot_storer.StoreItem(snap, dbUrl, dbName, snapshotsCollection)
-		}(msg)
-	}
 }
