42e85e58604634c261fbcbca8c4fa4bbd0f5a813
Jonathan Rosenboim <masterfqf@gmail.com>
2018-12-17 11:35:24 +0200

    Creates the snapshotStorer interface.

--- storer/snapshot_storer.go
+++ storer/snapshot_storer.go
@@ -4,6 +4,21 @@ import (
 	"github.com/Ripolak/reddit-snapshots/catcher"
 )
 
+type snapshotStorer interface {
+	StoreItem(subreddit catcher.SubredditSnapshot)
+}
+
+type DatabaseStorer struct {
+	MongoClient MongoClient
+	DbName      string
+	Collection  string
+}
+
+func (d DatabaseStorer) StoreItem(subreddit catcher.SubredditSnapshot) {
+	data := subreddit.ToBsonM()
+	d.MongoClient.insertToDatabase(d.DbName, d.Collection, data)
+}
+
 func StoreItem(subreddit catcher.SubredditSnapshot, mongoUrl string, dbName string, collection string) {
 	c := MongoClient{
 		Url: mongoUrl,
