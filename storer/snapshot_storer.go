9854d0847cddf03024155a9f5634088dba396a69
Jonathan Rosenboim <masterfqf@gmail.com>
2018-12-17 12:52:59 +0200

    Clean code fixes.

--- storer/snapshot_storer.go
+++ storer/snapshot_storer.go
@@ -18,11 +18,3 @@ func (d DatabaseStorer) StoreItem(subreddit catcher.SubredditSnapshot) {
 	data := subreddit.ToBsonM()
 	d.MongoClient.insertToDatabase(d.DbName, d.Collection, data)
 }
-
-//func StoreItem(subreddit catcher.SubredditSnapshot, mongoUrl string, dbName string, collection string) {
-//	c := mongoClient{
-//		Url: mongoUrl,
-//	}
-//	data := subreddit.ToBsonM()
-//	c.insertToDatabase(dbName, collection, data)
-//}
